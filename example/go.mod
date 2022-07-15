83d0771e6058d2838d56a5f5da66dbbf6e7735ed
ningfei <1056593544@qq.com>
2022-07-15 12:47:40 +0800

    version 1

---
+++ example/go.mod
@@ -0,0 +1,5 @@
+module example
+
+go 1.18
+
+require github.com/dinofei/gomod/a v0.1.0 // indirect
---
+++ example/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/dinofei/gomod/a"
+
+func main() {
+	a.Funca()
+}
